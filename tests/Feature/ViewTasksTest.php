9e3745a39e76221e25dd601dc71e94f88c8c2039
John Alcher <johnalcherdoloiras@gmail.com>
2019-05-05 01:17:55 +0800

    Implement a rudimentary "show" route for a Task

---
+++ tests/Feature/ViewTasksTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ViewTasksTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_view_a_task()
+    {
+        $task = factory(Task::class)->create();
+
+        $response = $this->json('get', '/api/tasks/'.$task->id);
+
+        $response->assertJson([
+            'title' => $task->title,
+            'description' => $task->description,
+            'is_finished' => $task->is_finished,
+        ]);
+    }
+}
